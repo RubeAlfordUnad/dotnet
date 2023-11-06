eac97b734ab39838f6e41d740f864d603ab6514c
RubeAlfordUnad <102335329+RubeAlfordUnad@users.noreply.github.com>
2023-11-05 21:08:23 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Humanizer;
+
+
+Console.WriteLine("Please insert a name");
+var name = Console.ReadLine();
+Console.WriteLine("Please insert your charge");
+var charge = Console.ReadLine();
+Console.WriteLine("Please insert your age");
+var age = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Hi, My name is {name}, my charge is {charge} and I´m {age.ToWords()} years old");
